38573f1104301d5ad58537e81af2f78f17b0d644
thakkarnetram <thakkarnetram10@gmail.com>
2023-12-28 19:25:09 +0530

    new questions added

--- questions.cpp
+++ questions.cpp
@@ -32,6 +32,7 @@ int multipleIfElseGreatestOfThreeNum() {
     return 0;
 }
 
+// NESTED IF ELSE
 int nestedIfElseGreatestTwoNum() {
     cout << "a program that will find the greatest of three numbers (use nested if -\n""else)" << endl;
     int n1, n2, n3;
@@ -65,11 +66,164 @@ int greatestOfThreeNumbers() {
     return 0;
 }
 
+// PROGRAM USING NESTED IF CONDITIONS TO ASSIGN GRADES ACCORDING TO USER INPUT
+/* @marks >= 90         "A"
+ * @marks >= 80 && < 90 "B"
+ * @marks >= 70 && <80  "C"
+ * @marks >= 60 && < 70 "D"
+ * @marks <  60        "Fail"
+ */
+int programToAssignGrade() {
+    int marks;
+    cout
+            << "A program using nested if condition that will enter total marks and assign grades according to the following"
+            << endl;
+    cin >> marks;
+//    Own Code
+    if (marks >= 90)
+        cout << "Grade A" << endl;
+    else if (marks >= 80 && marks < 90)
+        cout << "Grade B" << endl;
+    else if (marks >= 70 && marks < 80)
+        cout << "Grade C" << endl;
+    else if (marks >= 60 && marks < 70)
+        cout << "Grade D" << endl;
+    else if (marks < 60)
+        cout << "Fail" << endl;
+
+//    Text Book Code
+/*    if (marks >= 90) {
+        cout << "A Grade";
+    } else if (marks >= 80) {
+        cout << "B Grade";
+    } else if (marks >= 70) {
+        cout << "C Grade";
+    } else if (marks >= 60) {
+        cout << "D Grade";
+    } else {
+        cout << "Fail";
+    }
+*/
+    return 0;
+}
+
+// SWITCH STATEMENTS BASIC Q-1
+int switchStatementBasicProgram() {
+    cout << "A program to show the use of switch statement" << endl;
+    char x;
+    cin >> x;
+    switch (x) {
+        case 'a':
+        case 'A':
+            cout << "Vowel a" << endl;
+            break;
+        case 'e':
+        case 'E':
+            cout << "Vowel e " << endl;
+            break;
+        case 'o' :
+        case 'O' :
+            cout << "Vowel o" << endl;
+            break;
+        case 'i' :
+        case 'I' :
+            cout << "Vowel i";
+            break;
+        case 'u' :
+        case 'U' :
+            cout << "Vowel u";
+            break;
+        default:
+            cout << "Not a Vowel " << x << endl;
+    }
+    return 0;
+}
+
+//SWITCH STATEMENTS Q-2
+/* @Grade A - 15k Salary
+ * @Grade B - 12k Salary
+ * @Grade C - 10k Salary
+ * @Grade D - 09k Salary
+ * @Default - Invalid Grade*/
+int displaySalaryUsingSwitchCase() {
+    cout
+            << "A program that will accept a one character grade code and depending on the grade code display the basic salary according to the table given below"
+            << endl;
+    char grade;
+    cin >> grade;
+    switch (grade) {
+        case 'A':
+            cout << " 15k Salary " << endl;
+            break;
+        case 'B':
+            cout << " 12k Salary " << endl;
+            break;
+        case 'C':
+            cout << " 10k Salary " << endl;
+            break;
+        case 'D':
+            cout << " 9k Salary " << endl;
+            break;
+        default:
+            cout << " Invalid Grade Input " << endl;
+    }
+    return 0;
+}
+
+// LOOPS
+// while loop # 1
+// A program to find the sum of the first N natural number
+int whileLoopQuestion() {
+    int n;
+    int total = 0;
+    n = 1;
+    while (n <= 10) {
+        total += n;
+        n++;
+    }
+    cout << "Sum of the numbers  " << total << endl;
+    return 0;
+}
+
+// do while loop #2
+// A program to find the sum of the first N natural number
+int doWhileLoopQuestion() {
+    int N, number, sum;
+    cin >> N;
+    sum = 0;
+    number = 1;
+    do {
+        sum += number;
+        number += 1;
+    } while (number <= N);
+    {
+        cout << sum;
+    }
+    return 0;
+}
+
+// FOR LOOP #3
+// A program to print N Numbers
+int forLoopQuestion() {
+    int x;
+    cin>>x;
+    for (int i = 0; i <= x; i++) {
+        cout<<i<<endl;
+    }
+    return 0;
+}
+
 int main() {
     cout << "Some Easy Questions xD" << endl;
-    greatestOfTwoNumbers();
-    multipleIfElseGreatestOfThreeNum();
-    nestedIfElseGreatestTwoNum();
-    greatestOfThreeNumbers();
+//    greatestOfTwoNumbers();
+//    multipleIfElseGreatestOfThreeNum();
+//    nestedIfElseGreatestTwoNum();
+//    greatestOfThreeNumbers();
+//    programToAssignGrade();
+//    switchStatementBasicProgram();
+//    displaySalaryUsingSwitchCase();
+//    whileLoopQuestion();
+//    doWhileLoopQuestion();
+//    forLoopQuestion();
     return 0;
 }
