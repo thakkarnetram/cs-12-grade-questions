1f9a9b56b79e92f8dbc6668a85b7044ad483d90f
thakkarnetram <thakkarnetram10@gmail.com>
2023-12-30 02:15:01 +0530

    added some comments in filesQuestions.cpp

--- Ch-21 Files/filesQuestions.cpp
+++ Ch-21 Files/filesQuestions.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include<fstream>
 
+// This is called output stream where stream is the flow of data and requires a fstream header file.
+/*Writing data on the datafile:
+ *  keyboard → memory → hard disk/ storage device
+ *  This is called output stream where stream is the flow of data and requires an
+    ofstream.h header file
+*/
+/*Reading data from datafile:
+ * datafile → memory → output device (screen) or external storage device (hard disk/storage device)
+ * This is called input stream and requires ifstream.h header file. If both input
+   stream and output stream are used in the same program then header file
+   fstream.h is required*/
+/*Opening a file ( 2 ways )
+ * using constructor function of a class or using the member function open ( ) of the class. */
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
